4d8de793e6b7c6b2b07e632498be4dad6ab7abd7
PedroZilves <rm99482@fiap.com.br>
2023-04-18 11:47:35 -0300

    Deu tudo errado

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -31,3 +31,19 @@
 //     console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
 //     }
 // })
+function soma(p1,p2,p3){
+    nr1 = p1;
+    nr2 = p2;
+
+    return (parseInt(nr1)+parseInt(nr2)+ parseInt(p3))
+
+    // console.log(`Resultado: ${parseInt(nr1)+parseInt(nr2)+ parseInt(parametro)}`)
+    // console.log(`Este é o parametro que foi passado ${parametro}`)
+}
+
+//ARROW FUNCTION 
+const mudaCor = ()=>{
+    //função SET TIME OUT
+    setTimeout(alert("EXECUTOU"),5000)
+}
+mudaCor()
