f8edf9a2eb009c0644278c12a21556e0b9f1a3b7
PedroZilves <rm99482@fiap.com.br>
2023-04-18 10:58:12 -0300

    spread + html Colec + map mas deu errado pedir aju

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,16 +1,33 @@
-// const liElement = document.getElementsByTagName("li");
+// 
 // for (let index = 0; index<liElement.length; index++){
 //     console.log(liElement[index].textContent)
 // }
 // console.log(liElement)
-let nr1 = [1,2,3,4,5]
-let nr2 = [6,7,8,9,10]
-let nr3 = [nr1+nr2]
-console.log(`Array1-${nr1}`)
-console.log(`Array2-${nr2}`)
-console.log(`Array3-${nr3}`)
+// let nr1 = [1,2,3,4,5]
+// let nr2 = [6,7,8,9,10]
+// let nr3 = [...nr1,...nr2]
+// console.log(`Array1-${nr1}`)
+// console.log(`Array2-${nr2}`)
+// console.log(`Array3-${nr3}`)
 
-///Listando um array tecnica especial
-nr3.forEach((itemdDoaArray)=>{
-    console.log(`Array3- sendo listado ${itemdDoaArray}`)
-})
+// ///Listando um array tecnica especial
+// // nr3.forEach((itemdDoaArray)=>{
+// //     console.log(`Array3- sendo listado ${itemdDoaArray}`)
+// // })
+// //Listando o array com map
+// nr3.map((numero,key)=>{
+
+//   console.log(`${(key+1)} - Itens do array - ${numero}`);
+    
+
+// })
+
+//convertendo HTMLCollecion em array de forma indireta
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml];  
+// console.log(liElementArray)
+// liElementArray.map((item,key)=>{
+//     if (item.textContent == "item-39"){
+//     console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
+// })
